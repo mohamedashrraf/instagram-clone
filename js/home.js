3a77b545889638c4c57fc7e088f91608f9eda972
MohaMed AshRaf <48410724+mohamedashrraf@users.noreply.github.com>
2023-08-26 22:21:45 +0300

    home.js

--- js/home.js
+++ js/home.js
@@ -8,35 +8,35 @@ const postButton = document.getElementById('post-button');
 const postsContainer = document.querySelector('.posts');
 
 popupTrigger.addEventListener('click', () => {
-    popup.style.display = 'block';
+  popup.style.display = 'block';
 });
 
 closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-    uploadedImageContainer.innerHTML = ''; 
+  popup.style.display = 'none';
+  uploadedImageContainer.innerHTML = '';
 });
 
 imageUploader.addEventListener('change', () => {
-    const files = imageUploader.files;
-    for (const file of files) {
-        const image = document.createElement('img');
-      image.src = URL.createObjectURL(file);
-      image.style.width ="523px"
-        image.classList.add('uploaded-image'); 
-        uploadedImageContainer.appendChild(image);
-    }
+  const files = imageUploader.files;
+  for (const file of files) {
+    const image = document.createElement('img');
+    image.src = URL.createObjectURL(file);
+    image.style.width = "523px"
+    image.classList.add('uploaded-image');
+    uploadedImageContainer.appendChild(image);
+  }
 });
 
-let userLog = localStorage.getItem('userLog'); 
+let userLog = localStorage.getItem('userLog');
 let postElement;
 postButton.addEventListener('click', () => {
-    const postContent = postInput.value;
+  const postContent = postInput.value;
   const imageValue = imageUploader.value;
 
-    if (imageValue.trim() !== '') {
-      postElement = document.createElement('div');
-      postElement.classList.add("card");
-      postElement.innerHTML = `<div>
+  if (imageValue.trim() !== '') {
+    postElement = document.createElement('div');
+    postElement.classList.add("card");
+    postElement.innerHTML = `<div>
               <div class="top">
                 <div class="userDetails">
                   <div class="profilepic">
@@ -80,7 +80,7 @@ postButton.addEventListener('click', () => {
                     <i class="fa-regular fa-paper-plane fa-xl"></i>
                   </div>
                   <div class="right">
-                                        <i class="fa-regular fa-bookmark fa-xl"></i>
+                                        <i class="fa-regular fa-bookmark fa-xl" id="save-4" onclick="savePost(4)"></i>
 
                   </div>
                 </div>
@@ -91,7 +91,7 @@ postButton.addEventListener('click', () => {
                  <span class="likes" id="likee-4">0</span> likes
                 
                   <p class="message">
-                    <b>mohamed.ahmed</b>
+                    <b>${userLog}</b>
                   </p>
                 
                 <p class="card-text">${postContent}</p>
@@ -116,24 +116,24 @@ postButton.addEventListener('click', () => {
               </div>
             </div>`
 
-        const uploadedImages = uploadedImageContainer.querySelectorAll('.uploaded-image');
-        const imgBx = postElement.querySelector('.imgBx');
-        uploadedImages.forEach(image => {
-            const clonedImage = image.cloneNode();
-            clonedImage.classList.add('cover'); 
-            imgBx.appendChild(clonedImage); 
-        });
+    const uploadedImages = uploadedImageContainer.querySelectorAll('.uploaded-image');
+    const imgBx = postElement.querySelector('.imgBx');
+    uploadedImages.forEach(image => {
+      const clonedImage = image.cloneNode();
+      clonedImage.classList.add('cover');
+      imgBx.appendChild(clonedImage);
+    });
 
-        postsContainer.insertBefore(postElement, postsContainer.firstChild);
+    postsContainer.insertBefore(postElement, postsContainer.firstChild);
 
-        postInput.value = ''; 
-        popup.style.display = 'none'; 
-      uploadedImageContainer.innerHTML = ''; 
+    postInput.value = '';
+    popup.style.display = 'none';
+    uploadedImageContainer.innerHTML = '';
 
-      const deleteButton = postElement.querySelector('.fa-ellipsis-h'); 
-      deleteButton.addEventListener('click', () => {
-        deletePost(postElement);
-      });
+    const deleteButton = postElement.querySelector('.fa-ellipsis-h');
+    deleteButton.addEventListener('click', () => {
+      deletePost(postElement);
+    });
   }
 });
 
@@ -141,7 +141,7 @@ postButton.addEventListener('click', () => {
 //delete post
 
 function deletePost(deleteButton) {
-  const postElement = deleteButton.closest('.card'); 
+  const postElement = deleteButton.closest('.card');
   if (postElement) {
     postElement.remove();
   }
@@ -153,51 +153,69 @@ function deletePost(deleteButton) {
 let isLiked = false;
 let likeID;
 function addLike(num) {
-    const like = document.getElementById(`like-${num}`);  
-    
-    let likeText = document.getElementById(`likee-${num}`);
-    let likeCount = parseInt(likeText.textContent); 
+  const like = document.getElementById(`like-${num}`);
 
-    if (isLiked) {
-        like.classList.remove("fa-solid");
-        like.classList.add("fa-regular");
-        like.style.color = "black"; // Change to default color
-        likeCount--;
-    } else {
+  let likeText = document.getElementById(`likee-${num}`);
+  let likeCount = parseInt(likeText.textContent);
 
-        like.classList.add("fa-solid");
-        like.classList.remove("fa-regular");
-        like.style.color = "red";
-        likeCount++;
-        
+  if (isLiked) {
+    like.classList.remove("fa-solid");
+    like.classList.add("fa-regular");
+    like.style.color = "black"; // Change to default color
+    likeCount--;
+  } else {
 
-    }
-    likeText.textContent = likeCount;
+    like.classList.add("fa-solid");
+    like.classList.remove("fa-regular");
+    like.style.color = "red";
+    likeCount++;
 
-    isLiked = !isLiked;
+
+  }
+  likeText.textContent = likeCount;
+
+  isLiked = !isLiked;
 }
 
 
 
 ///add comment    ???
 function addComment(idNum) {
-    
+  let commentsAdded = document.getElementById(`commentsAdded-${idNum}`);
+  let textComment = document.getElementById(`textComment-${idNum}`);
 
-    let commentsAdded = document.getElementById(`commentsAdded-${idNum}`);
-    let textComment = document.getElementById(`textComment-${idNum}`);
+  commentsAdded.innerHTML += textComment.value + "<br>";
+  textComment.value = "";
+}
 
+//saved
+let isSaved = false;
+function savePost(num) {
+  const save = document.getElementById(`save-${num}`);
 
-        commentsAdded.innerHTML += textComment.value + "<br>";
-        textComment.value = "";
-    
+  if (isSaved) {
+    save.classList.remove("fa-solid");
+    save.classList.add("fa-regular");
+  } else {
+
+    save.classList.add("fa-solid");
+    save.classList.remove("fa-regular");
+  }
+
+  isSaved = !isSaved;
 }
 
 
 //log out
 
 const logout = document.getElementById("logout")
-logout.addEventListener("click",()=> {
-    window.location.href = "./index.html";
+logout.addEventListener("click", () => {
+  window.location.href = "./index.html";
+})
+//logout in mobile
+const logout2 = document.getElementById("logout2")
+logout2.addEventListener("click", () => {
+  window.location.href = "./index.html";
 })
 
 
@@ -236,18 +254,40 @@ let isDarkMode = false;
 
 dark.addEventListener("click", () => {
   isDarkMode = !isDarkMode;
-
   if (isDarkMode) {
     allElements.forEach(element => {
       element.style.backgroundColor = "black";
       element.style.color = "white";
-      //instaLogo.src = "images/insta_white.png";
+      // instaLogo.src = "images/insta_white.png";
     });
   } else {
     allElements.forEach(element => {
       element.style.backgroundColor = "";
       element.style.color = ""; // Reset to default color
-      //instaLogo.src = "images/logo.PNG";
+      // instaLogo.src = "images/logo.PNG";
+    });
+  }
+});
+
+////dark mode in mobile
+const dark2 = document.getElementById("dark2");
+const allElements2 = document.querySelectorAll("*");
+
+let isDarkMode2 = false;
+
+dark2.addEventListener("click", () => {
+  isDarkMode2 = !isDarkMode2;
+  if (isDarkMode2) {
+    allElements2.forEach(element => {
+      element.style.backgroundColor = "black";
+      element.style.color = "white";
+      // instaLogo.src = "images/insta_white.png";
+    });
+  } else {
+    allElements2.forEach(element => {
+      element.style.backgroundColor = "";
+      element.style.color = ""; // Reset to default color
+      // instaLogo.src = "images/logo.PNG";
     });
   }
 });
